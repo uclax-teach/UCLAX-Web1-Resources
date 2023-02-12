bcb2781fbc8cae2b39d4f92b8a99fc9785a6e403
Mitch Gohman <meatch@me.com>
2023-02-12 12:51:19 -0700

    Adding staff, tabbed and slideshows

---
+++ slideshow/slidesData.js
@@ -0,0 +1,33 @@
+import slide1 from './images/Core-Strength.jpg';
+import slide2 from './images/Staff-In-Action.jpg';
+import slide3 from './images/Senior.jpg';
+import slide4 from './images/Group-Picture.jpg';
+import slide5 from './images/Aerial-Open-Gym.jpg';
+
+export const slidesData = [
+    {
+        id: 1,
+        image: slide1,
+        title: 'Core Strength',
+    },
+    {
+        id: 2,
+        image: slide2,
+        title: 'Staff In Action',
+    },
+    {
+        id: 3,
+        image: slide3,
+        title: 'Senior',
+    },
+    {
+        id: 4,
+        image: slide4,
+        title: 'Group Picture',
+    },
+    {
+        id: 5,
+        image: slide5,
+        title: 'Aerial Open Gym',
+    },
+]
