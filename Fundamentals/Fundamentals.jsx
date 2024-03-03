7431cecf65f3067577677aa5ccfeb28d69d33804
Mitch Gohman <meatch@me.com>
2024-03-03 15:10:23 -0700

    s8 Fundamentals Resources

---
+++ Fundamentals/Fundamentals.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+// Scripts
+import "./scripts/index.js";
+
+// React Components
+
+const Fundamentals = () => {
+    return (
+        <FundamentalsStyled>
+            <h2>JavaScript Fundamentals</h2>
+        </FundamentalsStyled>
+    );
+};
+
+export default Fundamentals;
+
+const FundamentalsStyled = styled.div``;
